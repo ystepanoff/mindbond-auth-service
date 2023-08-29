60a5eb632a4e908118e754dbc2ad1cef3620f95c
Yegor Stepanov <ystepanoff@icloud.com>
2023-08-29 23:03:52 +0100

    Change start message

--- cmd/main.go
+++ cmd/main.go
@@ -34,7 +34,7 @@ func main() {
 		log.Fatalln("Failed to listing:", err)
 	}
 
-	fmt.Println("Auth Svc on", c.Port)
+	fmt.Println("Authentication Service on", c.Port)
 
 	s := services.Server{
 		H:   h,
